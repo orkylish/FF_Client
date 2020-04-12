97a8b2cf1f78f2c1a1737c3798c488ad1e84c885
Charles Snyder <scarles2010@gmail.com>
2020-04-12 15:47:55 -0700

    initial commit, contains some 10.98 otb edits like walkable shallows etc

---
+++ otclientrc.lua
@@ -0,0 +1,7 @@
+-- this file is loaded after all modules are loaded and initialized
+-- you can place any custom user code here
+
+EnterGame.setUniqueServer("forgottenfaction.com", 7171, 1098)
+
+print 'Startup done :]'
+
